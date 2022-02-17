a1a20311ff0ba6432c46f6ea71bc017f16157ad6
kingwangjjangs <bestbox1111@naver.com>
2022-02-17 22:43:39 +0900

    array customers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo"/>
-      <h2>kingwangjjangs shopping manage system</h2>
-    </div>
-  );
+
+import './App.css';
+import { Component } from 'react';
+import Customer from './components/Customer';
+
+
+const customers = [
+  {
+  'id':1,
+  'image':"https://placeimg.com/64/64/3",
+  'name':"홍길동",
+  'birthday':"810414",
+  'gender':"남자",
+  'job':"대학생"
+},
+{
+  'id':2,
+  'image':"https://placeimg.com/64/64/4",
+  'name':"박준형",
+  'birthday':"123456",
+  'gender':"남자",
+  'job':"중학생"
+},
+{
+  'id':3,
+  'image':"https://placeimg.com/64/64/5",
+  'name':"정말자",
+  'birthday':"330414",
+  'gender':"여자",
+  'job':"직장인"
+}
+
+]
+
+
+class App extends Component{
+  render(){
+    return(
+    
+      <div>
+        {
+          customers.map(c=>{
+            return(
+              
+            <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+        })
+        }
+
+      </div>
+
+    );
+  }
 }
 
 export default App;
